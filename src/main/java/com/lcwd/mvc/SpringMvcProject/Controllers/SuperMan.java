13320b42fbf593626984aca4d4a5274664a3021e
Vijay42-web <more.vijay42@gmail.com>
2024-02-15 10:52:26 +0530

    Initial commit

---
+++ src/main/java/com/lcwd/mvc/SpringMvcProject/Controllers/SuperMan.java
@@ -0,0 +1,35 @@
+package com.lcwd.mvc.SpringMvcProject.Controllers;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SuperMan 
+{
+	@RequestMapping("/about")
+	public String aboutRequestHandler() 
+	{
+		System.out.println("processing about request");
+		
+		return "about";
+	}
+
+	@RequestMapping("/services")
+	public String aboutServiceHandler() 
+	{
+		System.out.println("processing service request");
+		
+		return "services";
+	}
+	
+	
+	@RequestMapping("/backend")
+	public String aboutbackendHandler() 
+	{
+		System.out.println("processing backend request");
+		
+		return "backend";
+	}
+}
